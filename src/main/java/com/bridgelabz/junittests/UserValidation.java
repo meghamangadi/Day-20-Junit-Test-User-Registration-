c90c3ff785040940fce62813df01f8c8c9c17786
megha angadi <meghamangadi@gmail.com>
2021-09-16 00:44:48 +0530

    @Test

--- src/main/java/com/bridgelabz/junittests/UserValidation.java
+++ src/main/java/com/bridgelabz/junittests/UserValidation.java
@@ -1,7 +1,15 @@
-package com.bridgelabz.junittests; class UserValidation 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to JUnit Test User Validation Problems" );
-    }
+package com.bridgelabz.junittests;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserValidation {
+	
+public boolean firstName(String firstName) {
+
+	String regex = "^[A-Z]{1}[a-z]{2,}$";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(firstName);
+    return matcher.matches();
+}
 }
--- src/test/java/com/bridgelabz/junittests/UserValidationTest.java
+++ src/test/java/com/bridgelabz/junittests/UserValidationTest.java
@@ -1,20 +1,49 @@
 package com.bridgelabz.junittests;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class UserValidationTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class UserValidationTest {
+
+	UserValidation validation = new UserValidation();
+
+	@Test
+	public void GivenFirstName_When3CharacterWith1stUpperCaseLetter_shouldReturnTrue() {
+
+		boolean actualResult = validation.firstName("Megha");
+		Assert.assertEquals(true, actualResult);
+	}
+	
+	@Test
+	public void GivenFirstName_WhennAllLettersUpperCase_shouldReturnfalses() {
+
+		boolean actualResult = validation.firstName("MEGHA");
+		Assert.assertEquals(false, actualResult);
+	}
+	
+	@Test
+	public void GivenFirstName_When3CharacterWith2stUpperCaseLetter_shouldReturnfalse() {
+
+		boolean actualResult = validation.firstName("MeghA");
+		Assert.assertEquals(false, actualResult);
+	}
+	
+	@Test
+	public void GivenFirstName_When3CharacterWithnumericNumber_shouldReturnfalse() {
+
+		boolean actualResult = validation.firstName("Megha13");
+		Assert.assertEquals(false, actualResult);
+	}
+	@Test
+	public void GivenFirstName_When3CharacterWithout1stUpperCaseLetter_shouldReturnfalse() {
+
+		boolean actualResult = validation.firstName("megha");
+		Assert.assertEquals(false, actualResult);
+	}
+	@Test
+	public void GivenFirstName_When3CharacterWithnumericNumberSpecialCharacter_shouldReturnfalse() {
+
+		boolean actualResult = validation.firstName("Megha#1303");
+		Assert.assertEquals(false, actualResult);
+	}
 }
