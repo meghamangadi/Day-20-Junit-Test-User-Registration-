cd7c23813aa29cb96b31ef16c742a6193cdab583
megha angadi <meghamangadi@gmail.com>
2021-09-16 00:13:15 +0530

    Added: Welcome Message to the User

---
+++ src/main/java/com/bridgelabz/junittests/UserValidation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.junittests; class UserValidation 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to JUnit Test User Validation Problems" );
+    }
+}
---
+++ src/test/java/com/bridgelabz/junittests/UserValidationTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.junittests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UserValidationTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
