b348e928efd4e336891366de0a55379df7cb6f25
megha angadi <meghamangadi@gmail.com>
2021-09-19 23:36:10 +0530

    Added: Program for different Email samples with test cases for each samples

--- src/test/java/com/bridgelabz/junittests/EmailValidationTest.java
+++ src/test/java/com/bridgelabz/junittests/EmailValidationTest.java
@@ -26,18 +26,40 @@ public class EmailValidationTest {
 		boolean validationStatus = validation.email("Meghamangadi1203gmail.com");
 		Assert.assertEquals(false, validationStatus);
 	}
-	
+
 	@Test
 	public void GivenEmail_WhengivenwithMoreThanOnespecialcharacter___shouldReturnfalse() {
 
 		boolean validationStatus = validation.email("meghamangadi@#$!1203gmail.com");
 		Assert.assertEquals(false, validationStatus);
 	}
-	
+
 	@Test
 	public void GivenEmail_WhengivenwithTwoDots__shouldReturnfalse() {
 
 		boolean validationStatus = validation.email("megha.mangadi1203gmail.com");
 		Assert.assertEquals(false, validationStatus);
+
 	}
+
+	@Test
+	public void givenEmail_WhenAtProperPlace_ShouldReturnTrue() {
+		boolean result = validation.email("abc@gmail.com.com");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenEmail1_WhenAtNotProperPlace_ShouldReturnFalse() {
+		boolean result = validation.email("abc()*@gmail.com");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+    public void givenEmail1_WhenProper_ShouldReturnTrue() {
+        boolean result =  validation.email("abc.100@abc.com.au");
+        Assert.assertEquals(true, result);
+    }
+	
+
+
 }
