84104591d6b8fde5e894a322067c8c5d84926f76
megha angadi <meghamangadi@gmail.com>
2021-09-19 22:59:02 +0530

    Added: Program of Parameterised Test to validate multiple entry for the Email Address

---
+++ src/main/java/com/bridgelabz/junittests/ParametrisedEmailValidation.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.junittests;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ParametrisedEmailValidation {
+
+	public boolean email(String email) {
+		String regex = "^[a-z]{3,}([_+-.]?[a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+([.,][a-z]{2,3}+)*$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+
+} 
