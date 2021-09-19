2fbf15f4c721c96a7bb25c3d623478830184674a
megha angadi <meghamangadi@gmail.com>
2021-09-19 19:37:13 +0530

    Added: Program to Validate Password with Minimum 8 characters and one upper case letter and one numeric value and Specialcharacter ,entered by the user with different test cases

--- src/test/java/com/bridgelabz/junittests/PasswordValidationTest.java
+++ src/test/java/com/bridgelabz/junittests/PasswordValidationTest.java
@@ -7,37 +7,37 @@ public class PasswordValidationTest {
 	PasswordValidation password = new PasswordValidation();
 
 	@Test
-	public void GivenPasWithPassword_WhenMinimum8CharactersAndOneUpperCaseLetterAndOneNumericValue_shouldReturnTrue() {
+	public void GivenPasWithPassword_WhenMinimum8CharactersAndOneUpperCaseLetterAndOneNumericValueAndCharacter_shouldReturnTrue() {
 
-		boolean validationStatus = password.validatePassword("MeghaAngadi1203");
+		boolean validationStatus = password.validatePassword("Meghaangadi@1203");
 		Assert.assertEquals(true, validationStatus);
 	}
 
 	@Test
-	public void GivenPasWithPassword_WhenLessThen8CharactersWithNoUpperCaseLetterNoNumber_shouldReturnFalse() {
+	public void GivenPasWithPassword_When8CharactersWithNoUpperCaseLetterNoNumberNocharacter_shouldReturnFalse() {
 
-		boolean validationStatus = password.validatePassword("megha");
+		boolean validationStatus = password.validatePassword("meghaangadi");
 		Assert.assertEquals(false, validationStatus);
 	}
 
 	@Test
 	public void GivenPasWithPassword_WhenLessThen8Characters_shouldReturnFalse() {
 
-		boolean validationStatus = password.validatePassword("Megha12");
+		boolean validationStatus = password.validatePassword("Megha@1");
 		Assert.assertEquals(false, validationStatus);
 	}
 
 	@Test
-	public void GivenPasWithPassword_whenMinimum8CharactersWithSpecialCharacter_shouldReturnFalse() {
+	public void GivenPasWithPassword_whenMinimum8CharactersWithNoSpecialCharacter_shouldReturnFalse() {
 
-		boolean validationStatus = password.validatePassword("1megha@#");
+		boolean validationStatus = password.validatePassword("Megha1203");
 		Assert.assertEquals(false, validationStatus);
 	}
 
 	@Test
-	public void GivenPasWithPassword_whenLessThen8CharactersWithNoUpperCaseLetterWithSpecialCharacter_shouldReturnFalse() {
+	public void GivenPasWithPassword_when8CharactersWithNoUpperCaseLetteNoNumberWithSpecialCharacter_shouldReturnFalse() {
 
-		boolean validationStatus = password.validatePassword("megh@#");
+		boolean validationStatus = password.validatePassword("meghaangadi@#");
 		Assert.assertEquals(false, validationStatus);
 	}
 }
