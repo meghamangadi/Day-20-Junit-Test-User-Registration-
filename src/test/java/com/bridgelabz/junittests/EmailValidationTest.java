65d7c748a3e871360788a99d99844bc24d09a5a9
megha angadi <meghamangadi@gmail.com>
2021-09-19 17:37:41 +0530

    Added: More Test cases to EmailValidation

--- src/test/java/com/bridgelabz/junittests/EmailValidationTest.java
+++ src/test/java/com/bridgelabz/junittests/EmailValidationTest.java
@@ -26,4 +26,18 @@ public class EmailValidationTest {
 		boolean validationStatus = validation.email("Meghamangadi1203gmail.com");
 		Assert.assertEquals(false, validationStatus);
 	}
+	
+	@Test
+	public void GivenEmail_WhengivenwithMoreThanOnespecialcharacter___shouldReturnfalse() {
+
+		boolean validationStatus = validation.email("meghamangadi@#$!1203gmail.com");
+		Assert.assertEquals(false, validationStatus);
+	}
+	
+	@Test
+	public void GivenEmail_WhengivenwithTwoDots__shouldReturnfalse() {
+
+		boolean validationStatus = validation.email("megha.mangadi1203gmail.com");
+		Assert.assertEquals(false, validationStatus);
+	}
 }
